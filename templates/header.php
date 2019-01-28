a24ee49da95d453e5b120602e1778015a336d4cf
Dane Miller <danemiller22@gmail.com>
2019-01-28 09:50:20 -0600

    relative paths reference root

--- templates/header.php
+++ templates/header.php
@@ -11,8 +11,8 @@ $timestamp = $date->format('ymdhis');
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<link rel="stylesheet" href="css/styles.css?v=<?php echo $timestamp; ?>">
-<script src="js/main.js?v=<?php echo $timestamp; ?>"></script>
+<link rel="stylesheet" href="/css/styles.css?v=<?php echo $timestamp; ?>">
+<script src="/js/main.js?v=<?php echo $timestamp; ?>"></script>
 </head>
 <body>
 
