4264c9182b1a25f90221b241ecc224beec66e54c
Dane Miller <danemiller22@gmail.com>
2019-03-17 17:19:22 -0500

    Update photo-gallery.php

--- pages/photo-gallery.php
+++ pages/photo-gallery.php
@@ -1,5 +1,6 @@
 <?php
 	echo $config['company']['h'];
+	//echo '<div style="height:12px;"></div>';
 	loadPhotosGDrive(array(
 		 $config['photos']['folder']['homepage']['id']	// Homepage Sub-Folder (Best Photos)
 		,$config['photos']['folder']['main']['id']		// Main GDrive Photo-Gallery Folder
