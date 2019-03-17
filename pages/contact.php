b4d4c6e05af16a13ab15ec776f8a4c64089d815c
Dane Miller <danemiller22@gmail.com>
2019-03-17 15:58:38 -0500

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -1,10 +1,10 @@
 <?php 
 
 // WUFOO FORM
-//loadWufooForm($config['photos']['wufoo']['contactus']['id']);
+loadWufooForm($config['photos']['wufoo']['contactus']['id']);
 
 // GOOGLE FORM
-loadGoogleForm($config['google']['forms']['contactus']['id'], $config['google']['forms']['contactus']['title'], '1100px');
+//loadGoogleForm($config['google']['forms']['contactus']['id'], $config['google']['forms']['contactus']['title'], '1100px');
 
 echo $config['contact']['p'];
 
