082121baeb57fd1c772fa019f9e4936d461c88e0
Dane Miller <danemiller22@gmail.com>
2019-03-17 14:06:31 -0500

    Update admin.php

--- pages/admin.php
+++ pages/admin.php
@@ -1,24 +1,27 @@
 <h3>
-	External resources used in this website:
+	External resources used by this website
 </h3>
 <p>
-	Tools:
+	External Services/Tools
 	<ul>
 	
-		<li><a target=_blank href="http://www.myfreetextures.com/excellent-faded-and-worn-floral-design-on-old-paper-or-parchment-texture">Parchment (site background) - www.freecreatives.com</a></li>
+		<li><a target=_blank href="https://drive.google.com/folderview?id=<?php echo $config['photos']['folder']['main']['id']; ?>">Google Drive Folder</a></li>
 		
-		<li><a target=_blank href="https://www.freepik.com/free-photos-vectors/background">Parchment (navigation background) - created by kjpargeter - www.freepik.com</a></li>
+		<li><a target=_blank href="https://littleapplerenfest.wufoo.com/admin/">Wufoo Forms Builder</a></li>
+		
+		<!--<li><a target=_blank href="https://docs.google.com/forms">Google Forms Builder</a></li>-->
+		<li><a target=_blank href="https://drive.google.com/folderview?id=1IC5smpc6qvhNzt1Yacls6D27hQBKJa22">Google Forms Builder</a></li>
 	
 	</ul>
 </p>
 
 <p>
-	Creative Commons:
+	Creative Commons Resources
 	<ul>
 	
-		<li><a target=_blank href="http://www.myfreetextures.com/excellent-faded-and-worn-floral-design-on-old-paper-or-parchment-texture">Parchment (site background) - www.freecreatives.com</a></li>
+		<li><a target=_blank href="http://www.myfreetextures.com/excellent-faded-and-worn-floral-design-on-old-paper-or-parchment-texture">Parchment (banner background) - www.freecreatives.com</a></li>
 		
-		<li><a target=_blank href="https://www.freepik.com/free-photos-vectors/background">Parchment (navigation background) - created by kjpargeter - www.freepik.com</a></li>
+		<!--<li><a target=_blank href="https://www.freepik.com/free-photos-vectors/background">Parchment (navigation background) - created by kjpargeter - www.freepik.com</a></li>-->
 	
 	</ul>
 </p>
