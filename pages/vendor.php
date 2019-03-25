508d39a447724e835a5efd5dcb323ae5c27a4eb6
Dane Miller <danemiller22@gmail.com>
2019-03-24 21:13:35 -0500

    Update vendor.php

--- pages/vendor.php
+++ pages/vendor.php
@@ -6,6 +6,6 @@ loadGoogleForm($config['google']['forms']['vendorapplication']);
 // WUFOO FORM
 //loadWufooForm($config['photos']['wufoo']['vendorapplication']);
 
-echo $config['contact']['p'];
+//echo $config['contact']['p'];
 
 ?>
